4dd9e1e50cceab79325bd9d01b272843ef8ef312
Mohammad Hasan Heydari <hasan84heydari@gmail.com>
2023-11-12 11:15:23 +0330

    add photo detail

--- oobacho/enapp/urls.py
+++ oobacho/enapp/urls.py
@@ -3,5 +3,7 @@ from django.urls import path,include
 from . import views
 urlpatterns = [
     path('',views.home,name="enhome"),
+    path('gallery/',views.gallery,name="gallery"),
+    path('photo-detail/',views.img_detail,name="photo-detail"),
     
 ]
--- oobacho/enapp/views.py
+++ oobacho/enapp/views.py
@@ -4,3 +4,9 @@ from django.shortcuts import render
 
 def home(request):
     return render(request,"indexEn.html",{})
+
+def gallery(request):
+    return render(request,"gallery.html",{})
+
+def img_detail(request):
+    return render(request,"photo-detail.html",{})
