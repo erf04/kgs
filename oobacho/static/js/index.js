34390f098da3b3a39f6bdceaa62d523028459c37
Mohammad Hasan Heydari <hasan84heydari@gmail.com>
2023-12-20 12:21:46 +0330

    responsive owl carousel

--- oobacho/static/js/index.js
+++ oobacho/static/js/index.js
@@ -30,10 +30,20 @@ owl.owlCarousel({
   autoplay: true,
   autoplayTimeout: 3000,
   autoplayHoverPause: true,
-});
-$(".play").on("click", function () {
-  owl.trigger("play.owl.autoplay", [1000]);
-});
-$(".stop").on("click", function () {
-  owl.trigger("stop.owl.autoplay");
+  responsiveClass: true,
+  responsive: {
+    0: {
+      items: 1,
+      nav: true,
+    },
+    600: {
+      items: 3,
+      nav: false,
+    },
+    1000: {
+      items: 5,
+      nav: true,
+      loop: false,
+    },
+  },
 });
