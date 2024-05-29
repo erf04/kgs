8fc945b435f07495308b667113b38baed01a95b4
Mohammad Hasan Heydari <hasan84heydari@gmail.com>
2024-05-29 19:34:03 +0430

    original products added

--- kgs/static/js/index.js
+++ kgs/static/js/index.js
@@ -54,7 +54,7 @@
 
 // $(document).ready(function() {
   $("#owl-demo").owlCarousel({
-    autoPlay: 3000, //Set AutoPlay to 3 seconds
+    autoPlay: 3000, // Set AutoPlay to 3 seconds
     nav: true,
     items : 3,
     itemsDesktop : [1199,3],
